e074f01bc6399226781fa4594f876e7c0e66332c
Tommy Wilczk <tommywilczek@utexas.edu>
2019-07-14 11:53:24 -0500

    Added styling to Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 
+import classes from './Layout.module.css';
+
 const layout = (props) => (
     <Fragment>
         <div>[Toolbar, Side Drawer, Backdrop]</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Fragment>
