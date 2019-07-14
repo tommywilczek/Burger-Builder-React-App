8016517abef13bdca313ce6e01fc3f7a49d8ff04
Tommy Wilczk <tommywilczek@utexas.edu>
2019-07-14 17:53:41 -0500

    Created OrderSummary modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+
+export default function OrderSummary(props) {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>
+        )});
+    return (
+        <Fragment>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to checkout?</p>
+        </Fragment>
+    )
+}
